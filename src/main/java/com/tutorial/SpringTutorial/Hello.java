6e31e09331de5fd261037589fecb8c15509cb5bb
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2024-11-10 19:40:53 -0500

    Added Hello.java for hello world

---
+++ src/main/java/com/tutorial/SpringTutorial/Hello.java
@@ -0,0 +1,12 @@
+package com.tutorial.SpringTutorial;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping
+    public String greet() {
+        return "Hello World from Spring Tutorial";
+    }
+}
