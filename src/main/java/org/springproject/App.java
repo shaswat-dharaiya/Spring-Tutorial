78c8093ea472518091156a8ce3a4f5fe69b694a4
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2024-11-16 12:36:21 -0500

    Manually adding Spring Depandency to a project

--- src/main/java/org/springproject/App.java
+++ src/main/java/org/springproject/App.java
@@ -1,5 +1,8 @@
 package org.springproject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
+
+//        Without spring
+//        Dev obj = new Dev();
+//        obj.build();
+
+//        With spring
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Dev obj = context.getBean(Dev.class);
+        obj.build();
     }
+
 }
---
+++ src/main/java/org/springproject/Dev.java
@@ -0,0 +1,7 @@
+package org.springproject;
+
+public class Dev {
+    public void build(){
+        System.out.println("Working on awesome project");
+    }
+}
