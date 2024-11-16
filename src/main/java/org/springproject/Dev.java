d17a6958e30c55e6e9026bdbeb7665bd9526d182
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2024-11-16 12:47:26 -0500

    Creating a class and not adding it to the bean configurations.

--- src/main/java/org/springproject/Dev.java
+++ src/main/java/org/springproject/Dev.java
@@ -1,6 +1,11 @@
 package org.springproject;
 
 public class Dev {
+
+    public Dev(){
+        System.out.println("Dev constructor");
+    }
+
     public void build(){
         System.out.println("Working on awesome project");
     }
---
+++ src/main/java/org/springproject/Laptop.java
@@ -0,0 +1,12 @@
+package org.springproject;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop constructor");
+    }
+
+    public void compile(){
+        System.out.println("Laptop class");
+    }
+}
